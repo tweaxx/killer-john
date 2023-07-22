851de3766b471bb9f07c2f4d4917dd54323d264e
tweaxx <twea.xx.art@gmail.com>
2023-07-22 19:57:32 +0300

    policeman shooting

--- Assets/Scripts/Policeman.cs
+++ Assets/Scripts/Policeman.cs
@@ -1,8 +1,17 @@
+using DG.Tweening;
+using TweaxxGames.JamRaid;
 using UnityEngine;
 
 public class Policeman : Unit
 {
     [SerializeField] private Visor visor;
+    [SerializeField] private ParticleSystem gun;
+
+    [Space]
+    [SerializeField] private float timeToShoot = 1f;
+    [SerializeField] private bool canShoot;
+
+    private Sequence _shootTween;
 
     protected override void Awake()
     {
@@ -19,7 +28,13 @@ public class Policeman : Unit
     {
         if (obj.TryGetComponent(out Player player))
         {
-            //Debug.Log($"{name} spotted Player!");
+            _shootTween?.Kill();
+
+            // if spotted 2nd time
+            if (_shootTween != null)
+                Shoot();
+            else
+                _shootTween = Utilities.DoActionDelayed(Shoot, timeToShoot);
         }
     }
 
@@ -28,5 +43,11 @@ public class Policeman : Unit
         if (visor == null) return;
 
 
+    }
+
+    private void Shoot()
+    {
+        gun.Play();
+        _shootTween = null;
     }
 }
